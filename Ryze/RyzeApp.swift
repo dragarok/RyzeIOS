4bc5d1c7acfc55d9c2dc6f1870bdfb46d3b1a716
alokehpdev <alok.regmi@ehp.dev>
2025-04-07 05:21:13 +0545

    Initial Commit

---
+++ Ryze/RyzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RyzeApp.swift
+//  Ryze
+//
+//  Created by Alok Regmi on 07/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct RyzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RyzeTests/RyzeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RyzeTests.swift
+//  RyzeTests
+//
+//  Created by Alok Regmi on 07/04/2025.
+//
+
+import Testing
+@testable import Ryze
+
+struct RyzeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
